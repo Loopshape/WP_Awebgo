10ea3ae4e7ef853b46f2582892346e5ac1808052
Arjuna Noorsanto <awebgo.net@gmail.com>
2015-10-09 23:16:09 +0200

    backup before template mods

--- wp-content/themes/looptheme/index.php
+++ wp-content/themes/looptheme/index.php
@@ -7,10 +7,15 @@
 </div>
 <?php get_search_form(); ?>
 <?php endif; ?>
-
 <?php while (have_posts()) : the_post();
 ?>
+<div class="messagebody">
+<?php if(has_post_thumbnail()) {
+?>
+<div class="indexlist post thumbnail"><?php the_post_thumbnail(); ?></div>
+<?php } ?>
 <?php get_template_part('templates/content', get_post_type() != 'post' ? get_post_type() : get_post_format()); ?>
+</div>
 <?php endwhile; ?>
 
 <?php the_posts_navigation(); ?>
