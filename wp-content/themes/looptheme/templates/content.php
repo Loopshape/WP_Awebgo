28ddfdd5e457e9030b6ec609ff873df3394daf28
Arjuna Noorsanto <awebgo.net@gmail.com>
2015-11-08 17:32:53 +0100

    setup thumbnails in postlist

--- wp-content/themes/looptheme/templates/content.php
+++ wp-content/themes/looptheme/templates/content.php
@@ -4,7 +4,11 @@
 	<?php the_title(); ?></a></h2>
 	<?php if(has_post_thumbnail()) {
 	?>
-	<div class="indexlist post thumbnail"><?php the_post_thumbnail(); ?></div>
+	<div class="indexlist post thumbnail">
+	<?php //the_post_thumbnail();
+		echo get_the_post_thumbnail( $page->ID, 'medium' );
+	?>
+	</div>
 	<?php } ?>
     <?php get_template_part('templates/entry-meta'); ?>
   </header>
